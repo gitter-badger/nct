6cca37bf160a3ede5880937ad2d31a00fc28d30f
Aditi <aditi@ncrux.com>
2016-05-24 16:34:11 +0530

    Added python code generator and test.py script
    Signed-off-by: Aditi <aditi@ncrux.com>

---
+++ examples/simple/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import simple_conf
+
+print "MYBOOL = ", simple_conf.config['NCONFIG_MYBOOL']
+print "MYSTRING = ", simple_conf.config['NCONFIG_MYSTRING']
+print "MYTRISTATE = ", simple_conf.config['NCONFIG_MYTRISTATE']
+print "MYINT = ", simple_conf.config['NCONFIG_MYINT']
+print "MYHEX = ", simple_conf.config['NCONFIG_MYHEX']
+
